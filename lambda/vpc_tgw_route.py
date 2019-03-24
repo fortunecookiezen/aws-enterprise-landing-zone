f8b9d83ccceea7e44497a2680dc122925e765d50
Brian Peterson <brian@petersons.com>
2019-03-24 14:53:25 -0500

    Updated to handle exceptions. MUST return a status to cfn regardless.

--- lambda/vpc_tgw_route.py
+++ lambda/vpc_tgw_route.py
@@ -21,8 +21,6 @@ import boto3
 import cfnresponse
 import helpers
 
-from botocore.exceptions import ClientError
-
 logger = helpers.get_console_logger()
 
 
@@ -34,14 +32,20 @@ def create_tgw_route(DestinationCidrBlock, TransitGatewayId, RouteTableId):
     :param RouteTableId: (String) The ID of the route table for the route.
     :return: (Boolean) Returns true if the request succeeds; otherwise, it returns an error.
     """
-    ec2client = boto3.client("ec2")
     logger.info(f"Creating Route DestinationCidrBlock: {DestinationCidrBlock}, TransitGatewayId: {TransitGatewayId} "
                 f"RouteTableId: {RouteTableId}")
-    result = ec2client.create_route(
-        DestinationCidrBlock=DestinationCidrBlock,
-        TransitGatewayId=TransitGatewayId,
-        RouteTableId=RouteTableId
-    )
+    result = {}
+    try:
+        ec2client = boto3.client("ec2")
+        result = ec2client.create_route(
+            DestinationCidrBlock=DestinationCidrBlock,
+            TransitGatewayId=TransitGatewayId,
+            RouteTableId=RouteTableId
+        )
+    except Exception as e:
+        logger.error(e)
+        result['Return'] = False
+        result['Error'] = e
     return result
 
 
@@ -51,19 +55,32 @@ def delete_tgw_route(DestinationCidrBlock, RouteTableId):
     :param RouteTableId: (String) The ID of the route table for the route.
     :return: (Boolean) Returns true if the request succeeds; otherwise, it returns an error.
     """
-    ec2client = boto3.client("ec2")
     logger.info(f"Deleting Route DestinationCidrBlock: {DestinationCidrBlock}, RouteTableId: {RouteTableId}")
-    result = ec2client.delete_route(
-        DestinationCidrBlock=DestinationCidrBlock,
-        RouteTableId=RouteTableId
-    )
+    result = {}
+    try:
+        ec2client = boto3.client("ec2")
+        result = ec2client.delete_route(
+            DestinationCidrBlock=DestinationCidrBlock,
+            RouteTableId=RouteTableId
+        )
+        # delete_route does not provide a result["Return"] attribute like the create_route method. So create it here
+        if result['ResponseMetadata']['HTTPStatusCode'] == 200:
+            result['Return'] = True
+        else:
+            result['Return'] = False
+    except Exception as e:
+        logger.error(e)
+        result['Return'] = False
+        result['Error'] = e
     return result
 
 
 def lambda_handler(event, context):
-    logger.debug(f"event: {event}")
-    logger.debug(f"context: {context}")
+    logger.info(f"event: {event}")
+    logger.info(f"context: {context}")
     logger.info(f"RequestType: {event['RequestType']}")
+
+    result = {'Return': False}
 
     # CREATE
     if event['RequestType'] == 'Create':
@@ -75,15 +92,13 @@ def lambda_handler(event, context):
 
     # UPDATE
     elif event['RequestType'] == 'Update':
-        # There is no update_route method. We'll delete it, then create it.
-        try:
-            result = delete_tgw_route(
-                DestinationCidrBlock=event['ResourceProperties']['DestinationCidrBlock'],
-                RouteTableId=event['ResourceProperties']['RouteTableId']
-            )
-        except ClientError as e:
-            # If the route doesnt exist, it will throw a ClientError except. Log it and keep moving
-            logger.error(e)
+        # There is no update_route method. We'll try to delete it, then create it.
+        # But since we aren't passed the old route information, the delete will probably
+        # fail and will create a new route below without deleting the old route
+        result = delete_tgw_route(
+            DestinationCidrBlock=event['ResourceProperties']['DestinationCidrBlock'],
+            RouteTableId=event['ResourceProperties']['RouteTableId']
+        )
 
         result = create_tgw_route(
             DestinationCidrBlock=event['ResourceProperties']['DestinationCidrBlock'],
@@ -100,8 +115,10 @@ def lambda_handler(event, context):
 
     # Send Result back to Cloudformation
     logger.info(f"{event['RequestType']} Route result - {result}")
-    if result['Return'] != True:
-        response = cfnresponse.FAILED
-    else:
-        response = cfnresponse.SUCCESS
+    response = cfnresponse.FAILED
+    try:
+        if result['Return'] == True:
+            response = cfnresponse.SUCCESS
+    except Exception as e:
+        logger.error(e)
     cfnresponse.send(event, context, response, result, "CustomResourcePhysicalID")
